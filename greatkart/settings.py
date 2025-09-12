1fa019c1d0f0df5572020635b4818e67da58ddb9
Mohammad Reza Alipour <you@example.com>
2025-09-12 12:27:42 -0400

    Add user authentication features including registration, login, and password reset functionality

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -131,3 +132,15 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587    
+EMAIL_HOST_USER = 'mohammadrezaalipou947@gmail.com'
+EMAIL_HOST_PASSWORD = 'alipour0511'
+EMAIL_USE_TLS = True
